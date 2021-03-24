6b2ae09447e81bc9d8333adf0fc0ce5064cd98c2
OlssonTim <tiol@csc.kth.se>
2021-03-24 20:00:30 +0100

    First smart contract!

---
+++ Storage.sol
@@ -0,0 +1,10 @@
+pragma solidity >=0.4.0 <0.7.0;
+contract Storage {
+    uint storedData;
+    function set(uint x) public {
+        storedData = x;
+    }
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
